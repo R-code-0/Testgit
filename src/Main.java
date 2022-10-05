4bcd16f1e7c372d4bebe81ece703a87bc5106a56
Busu <busurmanbek000@gmail.com>
2022-10-05 12:27:49 +0600

    Hello world!

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        String s = "instance check";
+        switch (s.getClass().getSimpleName()){
+            case "String" -> System.out.println("String");
+            case "int","Integer" -> System.out.println("Integer");
+            default -> System.out.println("Never used");
+        }
     }
 }
